b9bcb85c137a5232d93fbdd6c7cafc7c5e9a1aa7
Everson Otoni <everson.ott@gmail.com>
2024-06-20 21:55:02 -0300

    Implementando rotas CRUD, com o Dunossauro

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,65 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
-
-
-def test_read_root_deve_retornar_ok_ola_mundo():
-    client = TestClient(app)  # Organização
-
+def test_read_root_deve_retornar_ok_ola_mundo(client):
     response = client.get('/')  # Execução (Ação literal do teste)
 
     assert response.status_code == HTTPStatus.OK  # Afirmação (Garantia)
     assert response.json() == {'message': 'Olá Mundo'}
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'testusername',
+            'password': 'password',
+            'email': 'test@test.com',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+
+    assert response.json() == {
+        'username': 'testusername',
+        'email': 'test@test.com',
+        'id': 1,
+    }
+
+
+def test_read_users(client):
+    response = client.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'username': 'testusername',
+                'email': 'test@test.com',
+                'id': 1,
+            }
+        ]
+    }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'password': '123',
+            'username': 'testeusername2',
+            'email': 'teste@teste.com',
+            'id': 1,
+        },
+    )
+    assert response.json() == {
+        'username': 'testeusername2',
+        'email': 'teste@teste.com',
+        'id': 1,
+    }
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.json() == {'message': 'User deleted'}
